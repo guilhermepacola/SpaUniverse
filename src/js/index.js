b4d47a1211457d4821ce28891664b6eacd5abc99
guilhermepacola <gui.pfaccio@gmail.com>
2024-03-08 12:37:49 -0300

    update files

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import {Router} from "./routes.js"
+
+
+
+const router = new Router()
+
+router.add("/", "./pages/Home.html")
+router.add("/universe", "./pages/Universe.html")
+router.add("/exploration", "./pages/Exploration.html")
+router.add(404, "./pages/error.html")
+
+router.handle()
+window.onpopstate = () => router.handle()
+router.addEventListeners()
+
+
---
+++ src/js/routes.js
@@ -0,0 +1,52 @@
+
+export class Router {
+    routes = {}
+
+    add(routeName, page) {
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+
+        fetch(route)
+            .then(data => data.text())
+            .then(html => {
+                document.querySelector("#app").innerHTML = html
+            })
+
+        this.changeTitle(route)
+    }
+
+    changeTitle(route) {
+        const titleDocument = document.getElementById('titleDocument')
+        const textValue = route.split("/").pop().split(".")[0];
+        const bodybg = document.querySelector("body")
+
+        if (route) {
+            titleDocument.innerText = `${textValue}`
+        }
+        bodybg.className = ""
+        bodybg.classList.toggle(textValue)
+
+    }
+
+    addEventListeners() {
+        document.getElementById('page').forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault()
+                this.route(event);
+            });
+        });
+    }
+
+}
