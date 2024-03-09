bcf3bb8e027dc3d09634599bdc0a5bff6566c18e
guilhermepacola <gui.pfaccio@gmail.com>
2024-03-08 23:42:42 -0300

    update page ERROR - styles for Mobile and Desktop

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,8 @@ const router = new Router()
 router.add("/", "./pages/Home.html")
 router.add("/universe", "./../pages/Universe.html")
 router.add("/exploration", "./../pages/Exploration.html")
-router.add(404, "./../pages/error.html")
+router.add(404, "./../pages/Error.html")
 
 router.handle()
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
-
-
-
