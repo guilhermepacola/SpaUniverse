7003b98bb38b287c4b2307bafaf858ddd5f41a65
guilhermepacola <gui.pfaccio@gmail.com>
2024-03-08 23:13:04 -0300

    UpdateFiles to Desktop view

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,13 @@ import {Router} from "./routes.js"
 const router = new Router()
 
 router.add("/", "./pages/Home.html")
-router.add("/universe", "./pages/Universe.html")
-router.add("/exploration", "./pages/Exploration.html")
-router.add(404, "./pages/error.html")
+router.add("/universe", "./../pages/Universe.html")
+router.add("/exploration", "./../pages/Exploration.html")
+router.add(404, "./../pages/error.html")
 
 router.handle()
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
-window.onsubmit = () => router.route()
+
 
 
