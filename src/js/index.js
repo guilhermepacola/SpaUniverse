e700e37a698dc80804e309b62ae9e2d6f436b3c0
Guilherme Pacola <99981700+guilhermepacola@users.noreply.github.com>
2025-09-04 14:33:06 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,9 @@ import {Router} from "./routes.js"
 
 const router = new Router()
 
-router.add("/", "./pages/Home.html")
-router.add("/universe", "./../pages/Universe.html")
-router.add("/exploration", "./../pages/Exploration.html")
+router.add("SpaUniverse/", "./pages/Home.html")
+router.add("SpaUniverse/universe", "./../pages/Universe.html")
+router.add("SpaUniverse/exploration", "./../pages/Exploration.html")
 router.add(404, "./../pages/Error.html")
 
 router.handle()
